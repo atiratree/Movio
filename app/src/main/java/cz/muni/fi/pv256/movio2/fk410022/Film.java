69d7ee01b9f64717298e13deec01045a7a624587
suomiy <suomiy@gmail.com>
2016-10-11 09:18:19 +0200

    Homework 3
    - added film detail
    - lifecycle methods example log

---
+++ app/src/main/java/cz/muni/fi/pv256/movio2/fk410022/Film.java
@@ -0,0 +1,109 @@
+package cz.muni.fi.pv256.movio2.fk410022;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+/**
+ * Created by suomiy on 10/4/16.
+ */
+
+public class Film implements Parcelable {
+    private String title;
+    private String description;
+    private long releaseDate;
+    private double popularity;
+    private int coverPath;
+    private int backdrop;
+
+    public Film(Parcel pc) {
+        title = pc.readString();
+        description = pc.readString();
+        releaseDate = pc.readLong();
+        popularity = pc.readDouble();
+        coverPath = pc.readInt();
+        backdrop = pc.readInt();
+    }
+
+    public Film(String title, String description, long releaseDate, double popularity, int coverPath, int backdrop) {
+        this.title = title;
+        this.description = description;
+        this.releaseDate = releaseDate;
+        this.popularity = popularity;
+        this.coverPath = coverPath;
+        this.backdrop = backdrop;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public long getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(long releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public double getPopularity() {
+        return popularity;
+    }
+
+    public void setPopularity(double popularity) {
+        this.popularity = popularity;
+    }
+
+    public int getCoverPath() {
+        return coverPath;
+    }
+
+    public void setCoverPath(int coverPath) {
+        this.coverPath = coverPath;
+    }
+
+    public int getBackdrop() {
+        return backdrop;
+    }
+
+    public void setBackdrop(int backdrop) {
+        this.backdrop = backdrop;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int flags) {
+        parcel.writeString(title);
+        parcel.writeString(description);
+        parcel.writeLong(releaseDate);
+        parcel.writeDouble(popularity);
+        parcel.writeInt(coverPath);
+        parcel.writeInt(backdrop);
+    }
+
+    public static final Parcelable.Creator<Film> CREATOR = new Parcelable.Creator<Film>() {
+        public Film createFromParcel(Parcel pc) {
+            return new Film(pc);
+        }
+
+        public Film[] newArray(int size) {
+            return new Film[size];
+        }
+    };
+}
+
--- app/src/main/java/cz/muni/fi/pv256/movio2/fk410022/MainActivity.java
+++ app/src/main/java/cz/muni/fi/pv256/movio2/fk410022/MainActivity.java
@@ -2,6 +2,7 @@ package cz.muni.fi.pv256.movio2.fk410022;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -9,10 +10,12 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnItemClickListener {
+    private boolean isTablet = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        isTablet = getResources().getBoolean(R.bool.isTablet);
         RecyclerView mRecyclerView;
         RecyclerView.Adapter mAdapter;
         RecyclerView.LayoutManager mLayoutManager;
@@ -28,16 +31,63 @@ public class MainActivity extends AppCompatActivity {
         mLayoutManager = new LinearLayoutManager(this);
         mRecyclerView.setLayoutManager(mLayoutManager);
 
-        Movie[] movies = new Movie[]{new Movie("The Shawshank Redemption", R.drawable.the_shawshank_redemption),
-                new Movie("The Godfather", R.drawable.the_godfather),
-                new Movie("Whiplash", R.drawable.whiplash),
-                new Movie("Hunt for the Wilderpeople", R.drawable.hunt_for_the_wilderpeople),
-                new Movie("Spirited Away", R.drawable.spirited_away),
-                new Movie("Interstellar", R.drawable.interstellar)};
-        mAdapter = new MyAdapter(movies);
+        Film[] movies = new Film[]{
+                new Film("The Shawshank Redemption",
+                        getString(R.string.the_shawshank_redemption_description),
+                        1994, 4.858781,
+                        R.drawable.the_shawshank_redemption,
+                        R.drawable.the_shawshank_redemption_backdrop),
+                new Film("The Godfather",
+                        getString(R.string.the_godfather__description),
+                        2014, 4.117833,
+                        R.drawable.the_godfather,
+                        R.drawable.the_godfather_backdrop),
+                new Film("Whiplash",
+                        getString(R.string.whiplash_description),
+                        2014, 4.117833,
+                        R.drawable.whiplash,
+                        R.drawable.whiplash_backdrop),
+                new Film("Hunt for the Wilderpeople",
+                        getString(R.string.hunt_for_the_wilderpeople_description),
+                        2016, 3.776794,
+                        R.drawable.hunt_for_the_wilderpeople,
+                        R.drawable.hunt_for_the_wilderpeople_backdrop),
+                new Film("Spirited Away",
+                        getString(R.string.spirited_away_description),
+                        2001, 2.596589,
+                        R.drawable.spirited_away,
+                        R.drawable.spirited_away_backdrop),
+                new Film("Interstellar",
+                        getString(R.string.hunt_for_the_wilderpeople_description),
+                        2014, 11.710438,
+                        R.drawable.interstellar,
+                        R.drawable.interstellar_backdrop)};
+        mAdapter = new MyAdapter(movies, this);
         mRecyclerView.setAdapter(mAdapter);
+
+        findViewById(R.id.recycler_view);
     }
 
+    @Override
+    public void onItemClick(Film film) {
+        if (!isTablet) {
+            Intent intent = new Intent(this, FilmDetailActivity.class);
+            intent.putExtra(FilmDetailActivity.FILM_PARAM, film);
+            startActivity(intent);
+        } else {
+            FilmDetailFragment fragment = FilmDetailFragment.newInstance(film);
+            if (findViewById(R.id.fragment_container) == null) {
+                getFragmentManager().beginTransaction()
+                        .add(R.id.fragment_container, fragment).commit();
+            } else {
+                android.app.FragmentTransaction transaction = getFragmentManager().beginTransaction();
+                transaction.replace(R.id.fragment_container, fragment);
+                transaction.addToBackStack(null);
+                transaction.commit();
+            }
+
+        }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -62,10 +112,6 @@ public class MainActivity extends AppCompatActivity {
         Theme theme = myPrefs.getPrefTheme();
         theme = (theme == Theme.APP_THEME) ? Theme.MY_THEME : Theme.APP_THEME;
         myPrefs.savePrefTheme(theme);
-
-        // changeTheme activity
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
-        finish();
+        recreate();
     }
 }
